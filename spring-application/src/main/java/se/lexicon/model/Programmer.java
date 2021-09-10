423d134a5517a80ce6e69a63b3060a9d03a6bece
Abbe-Mohamud <Abbe-Mohamud>
2021-09-10 23:04:10 +0200

    DI Part 2 Annotations (XML)

--- spring-application/src/main/java/se/lexicon/model/Programmer.java
+++ spring-application/src/main/java/se/lexicon/model/Programmer.java
@@ -1,8 +1,11 @@
 package se.lexicon.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import se.lexicon.service.RandomSalaryService;
 import se.lexicon.service.SalaryService;
 
+@Component
 public class Programmer implements Employee {
 
 
@@ -10,7 +13,7 @@ public class Programmer implements Employee {
 
     public Programmer() {
     }
-
+    @Autowired
     public void setSalaryService(SalaryService salaryService) {
         this.salaryService = salaryService;
     }
--- spring-application/src/main/java/se/lexicon/model/SalesPerson.java
+++ spring-application/src/main/java/se/lexicon/model/SalesPerson.java
@@ -1,15 +1,16 @@
 package se.lexicon.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import se.lexicon.service.SalaryService;
 
-
+@Component
 public class SalesPerson implements  Employee{
 
     SalaryService salaryService;
 
-    public SalesPerson() {
-    }
 
+    @Autowired
     public SalesPerson(SalaryService salaryService) {
         this.salaryService = salaryService;
     }
